958aba08806437f60c1c78b35357f3d110f8c7f2
Enzo Acosta <enzoacostab@gmail.com>
2024-01-02 12:06:24 -0300

    some changes

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -12,7 +12,7 @@ export default async function ProjectsPage() {
 		.filter((p) => p.published)
 
 	return (
-		<div className="relative pb-64 sm:pb-36">
+		<div className="relative pb-64 sm:pb-52 xl:pb-36">
 			<Navigation />
 			<div className="px-6 pt-20 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32">
 				<div className="max-w-2xl mx-auto lg:mx-0">
